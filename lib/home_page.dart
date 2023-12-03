2e46d7893edfc18ab5d56c829b751fda5eab19a3
boglbbogl <75574246+boglbbogl@users.noreply.github.com>
2023-12-03 23:40:07 +0900

    end commit

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,6 +1,4 @@
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_custom_overflow/custom_ellipsis.dart';
 
 class HomePage extends StatelessWidget {
